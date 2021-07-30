630ea2673e0c6cfde5a2441390b3ff752e5dfa4f
zhaoxiaowei <zhaoxiaowei@efy-tech.com>
2021-07-30 15:00:02 +0800

    init commit

---
+++ LteModuleManagement.cpp
@@ -0,0 +1,5 @@
+//
+// Created by xiaowei on 2021/7/30.
+//
+
+#include "LteModuleManagement.hpp"
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+/**
+ * The Product interface declares the operations that all concrete products must
+ * implement.
+ */
+
+
+int main() {
+  std::cout << "App: Launched with the ConcreteCreator1.\n";
+  return 0;
+}
